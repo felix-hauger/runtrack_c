93fbcf0e1d06d50445e34cc275cb2979fee2222c
felix-hauger <felix.hauger@laplateforme.io>
2023-11-29 17:26:05 +0100

    Fix day1 job06 - strcmp
    Fix the while loop condition, by making sure there is one character at
    the index.

--- jour1/job06/my_strcmp.c
+++ jour1/job06/my_strcmp.c
@@ -2,9 +2,7 @@ int my_strcmp(char *s1, char *s2)
 {
     int i = 0;
 
-    int togo = sizeof(s1) > sizeof(s2) ? sizeof(1) : sizeof(2);
-
-    while (i < togo) {
+    while (s1[i] || s2[i]) {
         if (s1[i] > s2[i]) {
             return 1;
         } else if (s1[i] < s2[i]) {
